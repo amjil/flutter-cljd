ebdf939b4422e68e3b1fadaf14ccf0a176c15769
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2025-01-09 03:23:04 +0300

    feat: Add Button and AnyPadding widgets with comprehensive gesture and rendering support

---
+++ lib/button.dart
@@ -0,0 +1,444 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/gestures.dart';
+
+class Button extends StatefulWidget {
+  const Button({
+    super.key,
+    required this.child,
+    this.isEnabled = true,
+    this.onTap,
+    this.onTapDown,
+    this.onTapUp,
+    this.onTapCancel,
+    this.onDoubleTap,
+    this.onLongPress,
+    this.onSecondaryTap,
+    this.onSecondaryTapUp,
+    this.onSecondaryTapDown,
+    this.onSecondaryTapCancel,
+    this.onHighlightChanged,
+    this.onHover,
+    this.mouseCursor,
+    this.enableFeedback = false,
+    this.excludeFromSemantics = false,
+    this.focusNode,
+    this.canRequestFocus = true,
+    this.onFocusChange,
+    this.autofocus = false,
+    this.statesController,
+  });
+
+  final Widget? Function(ButtonContext) child;
+  final bool isEnabled;
+  final GestureTapCallback? onTap;
+  final GestureTapDownCallback? onTapDown;
+  final GestureTapUpCallback? onTapUp;
+  final GestureTapCallback? onTapCancel;
+  final GestureTapCallback? onDoubleTap;
+  final GestureLongPressCallback? onLongPress;
+  final GestureTapCallback? onSecondaryTap;
+  final GestureTapUpCallback? onSecondaryTapUp;
+  final GestureTapDownCallback? onSecondaryTapDown;
+  final GestureTapCallback? onSecondaryTapCancel;
+  final ValueChanged<bool>? onHighlightChanged;
+  final ValueChanged<bool>? onHover;
+  final MouseCursor? mouseCursor;
+  final bool enableFeedback;
+  final bool excludeFromSemantics;
+  final ValueChanged<bool>? onFocusChange;
+  final bool autofocus;
+  final FocusNode? focusNode;
+  final bool canRequestFocus;
+  final WidgetStatesController? statesController;
+
+  @override
+  ButtonState createState() => ButtonState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    final List<String> gestures = <String>[
+      if (onTap != null) 'tap',
+      if (onDoubleTap != null) 'double tap',
+      if (onLongPress != null) 'long press',
+      if (onTapDown != null) 'tap down',
+      if (onTapUp != null) 'tap up',
+      if (onTapCancel != null) 'tap cancel',
+      if (onSecondaryTap != null) 'secondary tap',
+      if (onSecondaryTapUp != null) 'secondary tap up',
+      if (onSecondaryTapDown != null) 'secondary tap down',
+      if (onSecondaryTapCancel != null) 'secondary tap cancel'
+    ];
+    properties
+        .add(IterableProperty<String>('gestures', gestures, ifEmpty: '<none>'));
+    properties
+        .add(DiagnosticsProperty<MouseCursor>('mouseCursor', mouseCursor));
+  }
+}
+
+class ButtonContext {
+  const ButtonContext({
+    required this.context,
+    required this.state,
+    this.localOffset,
+    this.globalOffset,
+  });
+
+  final BuildContext context;
+  final Set<WidgetState> state;
+  final Offset? localOffset;
+  final Offset? globalOffset;
+}
+
+class ButtonState extends State<Button>
+    with AutomaticKeepAliveClientMixin<Button> {
+  bool _hovering = false;
+  Offset? _cursorLocalOffset = null;
+  Offset? _cursorGlobalOffset = null;
+
+  late final Map<Type, Action<Intent>> _actionMap = <Type, Action<Intent>>{
+    ActivateIntent: CallbackAction<ActivateIntent>(onInvoke: activateOnIntent),
+    ButtonActivateIntent:
+        CallbackAction<ButtonActivateIntent>(onInvoke: activateOnIntent),
+  };
+  WidgetStatesController? internalStatesController;
+
+  void activateOnIntent(Intent? intent) {
+    if (widget.onTap != null) {
+      if (widget.enableFeedback) {
+        Feedback.forTap(context);
+      }
+      widget.onTap?.call();
+    }
+  }
+
+  void handleStatesControllerChange() {
+    // Force a rebuild to resolve widget.mouseCursor
+    setState(() {});
+  }
+
+  WidgetStatesController get statesController =>
+      widget.statesController ?? internalStatesController!;
+
+  void initStatesController() {
+    if (widget.statesController == null) {
+      internalStatesController = WidgetStatesController();
+    }
+    statesController.update(WidgetState.disabled, !enabled);
+    statesController.addListener(handleStatesControllerChange);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initStatesController();
+    FocusManager.instance
+        .addHighlightModeListener(handleFocusHighlightModeChange);
+  }
+
+  @override
+  void didUpdateWidget(Button oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.statesController != oldWidget.statesController) {
+      oldWidget.statesController?.removeListener(handleStatesControllerChange);
+      if (widget.statesController != null) {
+        internalStatesController?.dispose();
+        internalStatesController = null;
+      }
+      initStatesController();
+    }
+    if (enabled != isWidgetEnabled(oldWidget)) {
+      statesController.update(WidgetState.disabled, !enabled);
+      if (!enabled) {
+        statesController.update(WidgetState.pressed, false);
+      }
+      // Don't call widget.onHover because many widgets, including the button
+      // widgets, apply setState to an ancestor context from onHover.
+      updateHighlight(WidgetState.hovered,
+          value: _hovering, callOnHover: false);
+    }
+    updateFocusHighlights();
+  }
+
+  @override
+  bool get wantKeepAlive => false;
+
+  @override
+  void dispose() {
+    FocusManager.instance
+        .removeHighlightModeListener(handleFocusHighlightModeChange);
+    statesController.removeListener(handleStatesControllerChange);
+    internalStatesController?.dispose();
+    super.dispose();
+  }
+
+  void updateHighlight(WidgetState type,
+      {required bool value, bool callOnHover = true}) {
+    switch (type) {
+      case WidgetState.pressed:
+        statesController.update(WidgetState.pressed, value);
+      case WidgetState.hovered:
+        if (callOnHover) {
+          statesController.update(WidgetState.hovered, value);
+        }
+      default:
+        break;
+    }
+    switch (type) {
+      case WidgetState.pressed:
+        widget.onHighlightChanged?.call(value);
+      case WidgetState.hovered:
+        if (callOnHover) {
+          widget.onHover?.call(value);
+        }
+      default:
+        break;
+    }
+  }
+
+  void handleFocusHighlightModeChange(FocusHighlightMode mode) {
+    if (!mounted) {
+      return;
+    }
+    setState(() {
+      updateFocusHighlights();
+    });
+  }
+
+  bool get _shouldShowFocus {
+    return switch (MediaQuery.maybeNavigationModeOf(context)) {
+      NavigationMode.traditional || null => enabled && _hasFocus,
+      NavigationMode.directional => _hasFocus,
+    };
+  }
+
+  void updateFocusHighlights() {
+    final bool showFocus = switch (FocusManager.instance.highlightMode) {
+      FocusHighlightMode.touch => false,
+      FocusHighlightMode.traditional => _shouldShowFocus,
+    };
+    updateHighlight(WidgetState.focused, value: showFocus);
+  }
+
+  bool _hasFocus = false;
+  void handleFocusUpdate(bool hasFocus) {
+    _hasFocus = hasFocus;
+    // Set here rather than updateHighlight because this widget's
+    // (WidgetState) states include WidgetState.focused if
+    // the InkWell _has_ the focus, rather than if it's showing
+    // the focus per FocusManager.instance.highlightMode.
+    statesController.update(WidgetState.focused, hasFocus);
+    updateFocusHighlights();
+    widget.onFocusChange?.call(hasFocus);
+  }
+
+  void handleAnyTapDown(TapDownDetails details) {
+    _cursorGlobalOffset = details.globalPosition;
+    _cursorLocalOffset = details.localPosition;
+    statesController.update(WidgetState.pressed, true);
+    updateKeepAlive();
+    updateHighlight(WidgetState.pressed, value: true);
+  }
+
+  void handleTapDown(TapDownDetails details) {
+    handleAnyTapDown(details);
+    widget.onTapDown?.call(details);
+  }
+
+  void handleTapUp(TapUpDetails details) {
+    clearOffsets();
+    widget.onTapUp?.call(details);
+  }
+
+  void handleSecondaryTapDown(TapDownDetails details) {
+    handleAnyTapDown(details);
+    widget.onSecondaryTapDown?.call(details);
+  }
+
+  void handleSecondaryTapUp(TapUpDetails details) {
+    clearOffsets();
+    widget.onSecondaryTapUp?.call(details);
+  }
+
+  void handleTap() {
+    updateHighlight(WidgetState.pressed, value: false);
+    if (widget.onTap != null) {
+      if (widget.enableFeedback) {
+        Feedback.forTap(context);
+      }
+      widget.onTap?.call();
+    }
+  }
+
+  void handleTapCancel() {
+    clearOffsets();
+    widget.onTapCancel?.call();
+    updateHighlight(WidgetState.pressed, value: false);
+  }
+
+  void handleDoubleTap() {
+    updateHighlight(WidgetState.pressed, value: false);
+    widget.onDoubleTap?.call();
+  }
+
+  void handleLongPressStart(LongPressStartDetails details) {
+    _cursorGlobalOffset = details.globalPosition;
+    _cursorLocalOffset = details.localPosition;
+  }
+
+  void handleLongPress() {
+    if (widget.onLongPress != null) {
+      if (widget.enableFeedback) {
+        Feedback.forLongPress(context);
+      }
+      widget.onLongPress!();
+    }
+  }
+
+  void handleSecondaryTap() {
+    updateHighlight(WidgetState.pressed, value: false);
+    widget.onSecondaryTap?.call();
+  }
+
+  void handleSecondaryTapCancel() {
+    clearOffsets();
+    widget.onSecondaryTapCancel?.call();
+    updateHighlight(WidgetState.pressed, value: false);
+  }
+
+  void clearOffsets() {
+    _cursorLocalOffset = null;
+    _cursorGlobalOffset = null;
+  }
+
+  bool isWidgetEnabled(Button widget) {
+    return _primaryButtonEnabled(widget) || _secondaryButtonEnabled(widget);
+  }
+
+  bool _primaryButtonEnabled(Button widget) {
+    return widget.isEnabled &&
+        (widget.onTap != null ||
+            widget.onDoubleTap != null ||
+            widget.onLongPress != null ||
+            widget.onTapUp != null ||
+            widget.onTapDown != null);
+  }
+
+  bool _secondaryButtonEnabled(Button widget) {
+    return widget.isEnabled &&
+        (widget.onSecondaryTap != null ||
+            widget.onSecondaryTapUp != null ||
+            widget.onSecondaryTapDown != null);
+  }
+
+  bool get enabled => isWidgetEnabled(widget);
+  bool get _primaryEnabled => _primaryButtonEnabled(widget);
+  bool get _secondaryEnabled => _secondaryButtonEnabled(widget);
+
+  void handleMouseEnter(PointerEnterEvent event) {
+    _hovering = true;
+    _cursorGlobalOffset = event.position;
+    _cursorLocalOffset = event.localPosition;
+    if (enabled) {
+      handleHoverChange();
+    }
+  }
+
+  void handleMouseMove(PointerHoverEvent event) {
+    _cursorGlobalOffset = event.position;
+    _cursorLocalOffset = event.localPosition;
+  }
+
+  void handleMouseExit(PointerExitEvent event) {
+    _hovering = false;
+    clearOffsets();
+    // If the exit occurs after we've been disabled, we still
+    // want to take down the highlights and run widget.onHover.
+    handleHoverChange();
+  }
+
+  void handleHoverChange() {
+    updateHighlight(WidgetState.hovered, value: _hovering);
+  }
+
+  bool get _canRequestFocus {
+    return switch (MediaQuery.maybeNavigationModeOf(context)) {
+      NavigationMode.traditional || null => enabled && widget.canRequestFocus,
+      NavigationMode.directional => true,
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+
+    final MouseCursor effectiveMouseCursor =
+        WidgetStateProperty.resolveAs<MouseCursor>(
+      widget.mouseCursor ?? WidgetStateMouseCursor.clickable,
+      statesController.value,
+    );
+
+    return Actions(
+      actions: _actionMap,
+      child: Focus(
+        focusNode: widget.focusNode,
+        canRequestFocus: _canRequestFocus,
+        onFocusChange: handleFocusUpdate,
+        autofocus: widget.autofocus,
+        child: MouseRegion(
+          cursor: effectiveMouseCursor,
+          onEnter: handleMouseEnter,
+          onExit: handleMouseExit,
+          onHover: handleMouseMove,
+          child: DefaultSelectionStyle.merge(
+            mouseCursor: effectiveMouseCursor,
+            child: Semantics(
+              onTap: widget.excludeFromSemantics || widget.onTap == null
+                  ? null
+                  : handleTap,
+              onLongPress:
+                  widget.excludeFromSemantics || widget.onLongPress == null
+                      ? null
+                      : handleLongPress,
+              child: GestureDetector(
+                onTapDown: _primaryEnabled ? handleTapDown : null,
+                onTapUp: _primaryEnabled ? handleTapUp : null,
+                onTap: _primaryEnabled ? handleTap : null,
+                onTapCancel: _primaryEnabled ? handleTapCancel : null,
+                onDoubleTap:
+                    widget.onDoubleTap != null ? handleDoubleTap : null,
+                onDoubleTapCancel:
+                    widget.onDoubleTap != null ? clearOffsets : null,
+                onLongPressCancel:
+                    widget.onLongPress != null ? clearOffsets : null,
+                onLongPress:
+                    widget.onLongPress != null ? handleLongPress : null,
+                onLongPressStart:
+                    widget.onLongPress != null ? handleLongPressStart : null,
+                onLongPressUp: widget.onLongPress != null ? clearOffsets : null,
+                onSecondaryTapDown:
+                    _secondaryEnabled ? handleSecondaryTapDown : null,
+                onSecondaryTapUp:
+                    _secondaryEnabled ? handleSecondaryTapUp : null,
+                onSecondaryTap: _secondaryEnabled ? handleSecondaryTap : null,
+                onSecondaryTapCancel:
+                    _secondaryEnabled ? handleSecondaryTapCancel : null,
+                behavior: HitTestBehavior.opaque,
+                excludeFromSemantics: true,
+                child: widget.child(
+                  ButtonContext(
+                    context: context,
+                    state: statesController.value,
+                    localOffset: _cursorLocalOffset,
+                    globalOffset: _cursorGlobalOffset,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/padding.dart
@@ -0,0 +1,229 @@
+import 'dart:math' as math;
+import 'package:flutter/foundation.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+class RenderAnyPadding extends RenderShiftedBox {
+  EdgeInsetsGeometry _padding;
+  TextDirection? _textDirection;
+  EdgeInsets? _resolvedPaddingCache;
+
+  RenderAnyPadding(this._padding, this._textDirection, [RenderBox? child]) : super(child);
+
+  EdgeInsets get resolvedPadding {
+    if (_resolvedPaddingCache != null) return _resolvedPaddingCache!;
+    _resolvedPaddingCache = _padding.resolve(_textDirection);
+    return _resolvedPaddingCache!;
+  }
+
+  void _markNeedResolution() {
+    _resolvedPaddingCache = null;
+    markNeedsLayout();
+  }
+
+  EdgeInsetsGeometry get padding => _padding;
+  set padding(EdgeInsetsGeometry value) {
+    if (_padding == value) return;
+    _padding = value;
+    _markNeedResolution();
+  }
+
+  TextDirection? get textDirection => _textDirection;
+  set textDirection(TextDirection? value) {
+    if (_textDirection == value) return;
+    _textDirection = value;
+    _markNeedResolution();
+  }
+
+  @override
+  double computeMinIntrinsicWidth(double height) {
+    final EdgeInsets padding = resolvedPadding;
+    final double childWidth = child?.getMinIntrinsicWidth(
+      math.max(0.0, height - padding.vertical)
+    ) ?? 0.0;
+    return math.max(0.0, childWidth + padding.horizontal);
+  }
+
+  @override
+  double computeMaxIntrinsicWidth(double height) {
+    final EdgeInsets padding = resolvedPadding;
+    final double childWidth = child?.getMaxIntrinsicWidth(
+      math.max(0.0, height - padding.vertical)
+    ) ?? 0.0;
+    return math.max(0.0, childWidth + padding.horizontal);
+  }
+
+  @override
+  double computeMinIntrinsicHeight(double width) {
+    final EdgeInsets padding = resolvedPadding;
+    final double childHeight = child?.getMinIntrinsicHeight(
+      math.max(0.0, width - padding.horizontal)
+    ) ?? 0.0;
+    return math.max(0.0, childHeight + padding.vertical);
+  }
+
+  @override
+  double computeMaxIntrinsicHeight(double width) {
+    final EdgeInsets padding = resolvedPadding;
+    final double childHeight = child?.getMaxIntrinsicHeight(
+      math.max(0.0, width - padding.horizontal)
+    ) ?? 0.0;
+    return math.max(0.0, childHeight + padding.vertical);
+  }
+
+  @override
+  Size computeDryLayout(BoxConstraints constraints) {
+    final EdgeInsets padding = resolvedPadding;
+    if (child != null) {
+      final BoxConstraints innerConstraints = constraints.deflate(padding);
+      final Size childSize = child!.getDryLayout(innerConstraints);
+      return constraints.constrain(Size(
+        math.max(0.0, childSize.width + padding.horizontal),
+        math.max(0.0, childSize.height + padding.vertical),
+      ));
+    }
+    return constraints.constrain(Size(
+      math.max(0.0, padding.horizontal),
+      math.max(0.0, padding.vertical),
+    ));
+  }
+
+  @override
+  double? computeDistanceToActualBaseline(TextBaseline baseline) {
+    final EdgeInsets padding = resolvedPadding;
+    if (child == null) { 
+      return null;
+    }
+    final distance = child!.getDistanceToActualBaseline(baseline);
+    return distance == null ? null : distance + padding.top;
+  }
+
+  @override
+  void performLayout() {
+    final BoxConstraints constraints = this.constraints;
+    final EdgeInsets padding = resolvedPadding;
+
+    if (child != null) {
+      final BoxConstraints innerConstraints = constraints.deflate(padding);
+      child!.layout(innerConstraints, parentUsesSize: true);
+      final BoxParentData childParentData = child!.parentData as BoxParentData;
+      childParentData.offset = Offset(padding.left, padding.top);
+      size = constraints.constrain(Size(
+        math.max(0.0, child!.size.width + padding.horizontal),
+        math.max(0.0, child!.size.height + padding.vertical),
+      ));
+    } else {
+      size = constraints.constrain(Size(
+        math.max(0.0, padding.horizontal),
+        math.max(0.0, padding.vertical),
+      ));
+    }
+  }
+
+  @override
+  void debugPaintSize(PaintingContext context, Offset offset) {
+    super.debugPaintSize(context, offset);
+    if (kDebugMode) {
+      final Rect outerRect = offset & size;
+      debugPaintPadding(
+        context.canvas,
+        outerRect,
+        child != null ? resolvedPadding.deflateRect(outerRect) : null,
+      );
+    }
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<EdgeInsetsGeometry>('padding', padding));
+    properties.add(EnumProperty<TextDirection>('textDirection', textDirection));
+  }
+}
+
+class AnyPadding extends SingleChildRenderObjectWidget {
+  final EdgeInsetsGeometry padding;
+
+  const AnyPadding({
+    Key? key,
+    required this.padding,
+    Widget? child,
+  }) : super(key: key, child: child);
+
+  @override
+  RenderAnyPadding createRenderObject(BuildContext context) {
+    return RenderAnyPadding(
+      padding,
+      Directionality.maybeOf(context),
+    );
+  }
+
+  @override
+  void updateRenderObject(BuildContext context, RenderAnyPadding renderObject) {
+    renderObject
+      ..padding = padding
+      ..textDirection = Directionality.maybeOf(context);
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<EdgeInsetsGeometry>('padding', padding));
+  }
+}
+
+class AnimatedAnyPadding extends ImplicitlyAnimatedWidget {
+  final Widget? child;
+  final EdgeInsetsGeometry padding;
+
+  const AnimatedAnyPadding({
+    Key? key,
+    required this.padding,
+    this.child,
+    Curve curve = Curves.linear,
+    required Duration duration,
+    VoidCallback? onEnd,
+  }) : super(key: key, curve: curve, duration: duration, onEnd: onEnd);
+
+  @override
+  AnimatedAnyPaddingState createState() => AnimatedAnyPaddingState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<EdgeInsetsGeometry>('padding', padding));
+  }
+}
+
+class AnimatedAnyPaddingState extends AnimatedWidgetBaseState<AnimatedAnyPadding> {
+  EdgeInsetsGeometryTween? _padding;
+
+  @override
+  void initState() {
+    _padding = null;
+    super.initState();
+  }
+
+  @override
+  void forEachTween(TweenVisitor<dynamic> visitor) {
+    _padding = visitor(
+      _padding,
+      widget.padding,
+      (dynamic value) => EdgeInsetsGeometryTween(begin: value as EdgeInsetsGeometry),
+    ) as EdgeInsetsGeometryTween?;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnyPadding(
+      padding: _padding!.evaluate(animation),
+      child: widget.child,
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<EdgeInsetsGeometryTween>('padding', _padding, defaultValue: null));
+  }
+}
